03cb4ec5647a50a3f10b3e71b5e570c84a496815
mariavaz <jimenavaz@gmail.com>
2017-01-23 13:10:39 -0700

    Assignment

--- js/main.js
+++ js/main.js
@@ -33,61 +33,20 @@ window.onclick = function(event) {
 
 //WORK SLIDESHOW----------------------------------------------------------
 
-$(".column img").click(function(){
-	openModal();
-	var num = $(this).data("pic");
-	currentSlide(num);
-});
-
-function openModal() {
-  document.getElementById('myModal').style.display = "block";
-}
-
-function closeModal() {
-  document.getElementById('myModal').style.display = "none";
-}
-
-$(".modal .close").on("click", function(){
-	console.log("word");
-	$(this).parent().fadeOut();
-});
-
-$(".next").click(plusSlides(2));
-
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
-}
 
 //MEDIA MENU----------------------------------------------------------------------
 
-	//$('header nav ul li:first-child').click(function(){
-    //	$(this).parent().toggleClass('slideDown');
-  	//}
+function toggleMenu(){
+		var displayValue = $("nav").css("display");
+		if(displayValue === "none"){
+			$("nav").slideDown();
+		}	else {
+			$("nav").slideUp();
+		}
+}
+
+$(".hamburger").click(toggleMenu);
+
 
 //BACK TO TOP ARROW----------------------------------------------------------
 	
